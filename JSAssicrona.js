144c8e86ce441ece696d43bf702892e1cfe42111
Gabriel Malheiros <gabrielmalheirosmf@hotmail.com>
2022-05-21 15:17:09 -0300

    certo

--- JSAssicrona.js
+++ JSAssicrona.js
@@ -1,16 +1,15 @@
 var imputElement = document.querySelector('#cep')
 
 //fuction para pesquisar o cep
-
 function searchUser(){
-    axios.get(`https://viacep.com.br/ws/${inputElement.value}/json/`)
-.then(res =>{ 
+    axios.get(`https://viacep.com.br/ws/$(inpuntElement.value)/json/`)
+.then(res => { 
     document.getElementById('rua').value = res.data.logradouro 
     document.getElementById('bairro').value = res.data.bairro 
     document.getElementById('cidade').value = res.data.localidade 
     document.getElementById('estado').value = res.data.uf
- }) 
+    }) 
  .catch(() => 
  { alert('o cep $(imputElement.value) não existe!');
-}) 
-}
+    }) 
+};
